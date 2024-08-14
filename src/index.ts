8fc90572ef1c172b974d06ce067e8c0bef6c24ce
Ernest Brayce <ernest.brayce@nexum.com>
2024-08-14 12:47:33 +0000

    feat: can run project with custom filename

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ import { sendEmail } from './sendEmail';
 // Load environment variables from .env file
 dotenv.config();
 
+// Get template file name from command-line arguments or default to 'welcome.mjml'
+const templateFile = process.argv[2] || 'welcome.mjml';
+
 // Call the sendEmail function
-sendEmail();
+sendEmail(templateFile);
